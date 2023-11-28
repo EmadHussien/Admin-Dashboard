4c8cda0af67606772eae0d5059140bbaf655f3a6
Emad Hussien <emadhussien851@gmail.com>
2023-11-28 05:11:50 +0200

    Update WidgetSm.jsx

--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 export default function WidgetSm() {
   const { userRequests } = useUserRequests();
+  console.log("first");
   const [latsetUsers, setLatestUsers] = useState([
     {
       _id: 1,
@@ -43,7 +44,7 @@ export default function WidgetSm() {
         console.log(e);
       }
     }
-    if (latsetUsers) getLatest5Users();
+    if (latsetUsers[0]._id === 1) getLatest5Users();
   });
 
   return (
